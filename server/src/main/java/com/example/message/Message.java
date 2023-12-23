96eb583d8f13d38204f79da68a26630957df3ccd
feroager <you@example.com>
2023-12-23 22:24:10 +0100

    Added content to the Message class

--- server/src/main/java/com/example/message/Message.java
+++ server/src/main/java/com/example/message/Message.java
@@ -1,5 +1,59 @@
 package com.example.message;
 
-public class Message
-{
+import java.io.Serializable;
+
+/**
+ * Represents a message exchanged between the server and the client.
+ * Instances of this class are used to encapsulate communication details.
+ */
+public class Message implements Serializable {
+    /**
+     * Enumerates the different types of messages.
+     */
+    private final MessageType type;
+
+    /**
+     * Stores the data associated with the message.
+     */
+    private final String data;
+
+    /**
+     * Constructs a message with the specified type and no data.
+     *
+     * @param type The type of the message.
+     */
+    public Message(MessageType type) {
+        this.type = type;
+        this.data = null;
+    }
+
+    /**
+     * Constructs a message with the specified type and data.
+     *
+     * @param type The type of the message.
+     * @param data The data associated with the message.
+     */
+    public Message(MessageType type, String data) {
+        this.type = type;
+        this.data = data;
+    }
+
+    /**
+     * Gets the type of the message.
+     *
+     * @return The type of the message.
+     */
+    public MessageType getType() {
+        return type;
+    }
+
+    /**
+     * Gets the data associated with the message.
+     *
+     * @return The data associated with the message.
+     */
+    public String getData() {
+        return data;
+    }
 }
+
