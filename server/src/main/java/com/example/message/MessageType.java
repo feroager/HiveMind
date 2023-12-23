eb642a73caf57f83c788e44722fdfe37213d7dc2
feroager <you@example.com>
2023-12-23 22:10:22 +0100

    Added content to the MessageType Enumeration

--- server/src/main/java/com/example/message/MessageType.java
+++ server/src/main/java/com/example/message/MessageType.java
@@ -1,5 +1,39 @@
 package com.example.message;
 
-public enum MessageType
-{
+/**
+ * The enumeration defines the basic types of messages exchanged between the server and the client
+ */
+public enum MessageType {
+    /**
+     * The server requests a login and password
+     */
+    PASS_REQUEST,
+    /**
+     * The client sends the login and password.
+     */
+    USER_PASS,
+    /**
+     * The client requests registration and sends a login and password
+     */
+    REGISTER_REQUESTED,
+    /**
+     * The server informs that login was successful
+     */
+    PASS_ACCEPTED,
+    /**
+     * The server reports that login has failed
+     */
+    PASS_DENIED,
+    /**
+     * The server informs that the registration was successful
+     */
+    REGISTER_ACCEPTED,
+    /**
+     * The server reports that registration has failed
+     */
+    REGISTER_DENIED,
+    /**
+     * Regular text message
+     */
+    TEXT
 }
