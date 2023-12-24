23b1b316ba16f3bd4af5962df3d7140f25b1e3e5
feroager <you@example.com>
2023-12-24 19:04:00 +0100

    An abstract User class was created and added to the server and client modules. A child UserServer class was created in the server module and UserClient in the client module. Message and support packages copied to the client module

---
+++ client/src/main/java/com/example/support/UserClient.java
@@ -0,0 +1,9 @@
+package com.example.support;
+
+public class UserClient extends User
+{
+    public UserClient(String login, String password, String mail)
+    {
+        super(login, password, mail);
+    }
+}
