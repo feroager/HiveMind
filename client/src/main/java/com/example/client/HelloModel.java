dea4f3b44511203954b37cd826db7edd5e4b6f23
feroager <you@example.com>
2023-12-25 17:25:11 +0100

    Change class name from HelloApplication to HelloModel

--- client/src/main/java/com/example/client/HelloModel.java
+++ client/src/main/java/com/example/client/HelloModel.java
@@ -7,10 +7,10 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloModel extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("HelloView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloModel.class.getResource("HelloView.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setResizable(false);
