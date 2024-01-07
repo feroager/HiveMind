95a7aa445654fbd6d992f5e4eda9341762e6aefb
feroager <you@example.com>
2024-01-07 18:26:40 +0100

    Deleted User and UserClient in client module

--- client/src/main/java/com/example/utils/UserClient.java
+++
@@ -1,9 +0,0 @@
-package com.example.utils;
-
-public class UserClient extends User
-{
-    public UserClient(String login, String password, String mail)
-    {
-        super(login, password, mail);
-    }
-}
