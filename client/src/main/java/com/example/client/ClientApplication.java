8849592c9af6cb0334fcfa9bffa0c06c47bf9461
feroager <you@example.com>
2024-01-25 10:08:56 +0100

    Moved internal ClientHandler class to a separate file

--- client/src/main/java/com/example/client/ClientApplication.java
+++ client/src/main/java/com/example/client/ClientApplication.java
@@ -15,31 +15,6 @@ public class ClientApplication {
     private RegistrationController registrationController;
     private LoginController loginController;
 
-    private static class ClientHandler extends Thread {
-
-        private String serverIp;
-        private int serverPort;
-        private MainController mainController;
-        private FooterController footerController;
-        private Message message;
-
-        public ClientHandler(String serverIp, int serverPort, MainController mainController, Message message)
-        {
-            this.serverIp = serverIp;
-            this.serverPort = serverPort;
-            this.mainController = mainController;
-            this.footerController = mainController.getFooterController();
-            this.message=message;
-        }
-
-
-        @Override
-        public void run()
-        {
-            footerController.setUsernameLabelFooter(message);
-        }
-    }
-
     public ClientApplication(String serverIp, int serverPort) {
         this.serverIp = serverIp;
         this.serverPort = serverPort;
---
+++ client/src/main/java/com/example/client/ClientHandler.java
@@ -0,0 +1,29 @@
+package com.example.client;
+
+import com.example.message.Message;
+
+class ClientHandler extends Thread
+{
+
+    private String serverIp;
+    private int serverPort;
+    private MainController mainController;
+    private FooterController footerController;
+    private Message message;
+
+    public ClientHandler(String serverIp, int serverPort, MainController mainController, Message message)
+    {
+        this.serverIp = serverIp;
+        this.serverPort = serverPort;
+        this.mainController = mainController;
+        this.footerController = mainController.getFooterController();
+        this.message = message;
+    }
+
+
+    @Override
+    public void run()
+    {
+        footerController.setUsernameLabelFooter(message);
+    }
+}
