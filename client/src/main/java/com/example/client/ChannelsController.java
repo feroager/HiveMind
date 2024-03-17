06f09e8f97885f6b4b4fbdc893710ea5787efaf0
feroager <you@example.com>
2024-03-17 14:25:40 +0100

    Added showCreateChannelDialog method in ChannelsController

--- client/src/main/java/com/example/client/ChannelsController.java
+++ client/src/main/java/com/example/client/ChannelsController.java
@@ -2,15 +2,20 @@ package com.example.client;
 
 import com.example.database.models.Channel;
 import com.example.database.models.Message;
-import com.example.utils.ConsoleHelper;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Optional;
 
 import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
 
@@ -24,6 +29,7 @@ public class ChannelsController {
     private ClientHandler clientHandler;
     private List<Channel> channelList;
     private MessagesController messagesController;
+    private String nameNewlyCreatedChannel;
 
     /**
      * Sets the client handler for this controller.
@@ -44,11 +50,68 @@ public class ChannelsController {
             this.channelList = channelList;
             channelsContainer.getChildren().clear();
 
+            Button addChannelButton = new Button("Add channel");
+            addChannelButton.setPrefWidth(200);
+            addChannelButton.setMinHeight(USE_COMPUTED_SIZE);
+            addChannelButton.setMinWidth(USE_COMPUTED_SIZE);
+            addChannelButton.setMaxHeight(USE_COMPUTED_SIZE);
+            addChannelButton.setMaxWidth(USE_COMPUTED_SIZE);
+            addChannelButton.setOnAction(event -> showCreateChannelDialog());
+
+            channelsContainer.getChildren().add(addChannelButton);
+
             for (Channel channel : this.channelList) {
                 Button channelButton = createChannelButton(channel);
                 channelsContainer.getChildren().add(channelButton);
             }
         });
+    }
+
+    private void showCreateChannelDialog()
+    {
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.setTitle("Create a new channel");
+
+        Label messageLabel = new Label("Please enter the name for the new channel:");
+        messageLabel.setStyle("-fx-text-fill: white;");
+
+        TextField chamnnelNameField = new TextField();
+        chamnnelNameField.setPromptText("Enter channel name");
+        chamnnelNameField.setEditable(true);
+        chamnnelNameField.setVisible(true);
+        chamnnelNameField.setManaged(true);
+
+        VBox content = new VBox();
+        content.getChildren().addAll(messageLabel, chamnnelNameField);
+        content.layout();
+
+        dialog.getDialogPane().setContent(content);
+        dialog.getDialogPane().setBackground(new Background(new BackgroundFill(Color.valueOf("#343541"), CornerRadii.EMPTY, Insets.EMPTY)));
+
+        ButtonType createButtonType = new ButtonType("Create", ButtonBar.ButtonData.OK_DONE);
+        ButtonType backButtonType = new ButtonType("Back", ButtonBar.ButtonData.CANCEL_CLOSE);
+        dialog.getDialogPane().getButtonTypes().addAll(createButtonType, backButtonType);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+
+        if(result.get() == createButtonType)
+        {
+            String channelName = chamnnelNameField.getText();
+            logger.info("Pressed createButtonType");
+            createNewChannel(channelName);
+        }
+        else if(result.get() == backButtonType)
+        {
+            dialog.close();
+            logger.info("Pressed backButtonType");
+        }
+    }
+
+    private void createNewChannel(String channelName)
+    {
+        logger.info("Creating new channel: " + channelName);
+        this.nameNewlyCreatedChannel = channelName;
+        //clientHandler.setCreateNewServerRequest(true);
     }
 
     /**
